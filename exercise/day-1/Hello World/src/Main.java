c6216d6dcac8aeb2dda2f78b106601a2981d1881
Royzen Anggatama <72536@fif.co.id>
2025-09-03 15:33:51 +0700

    push 1

---
+++ exercise/day-1/Hello World/src/Main.java
@@ -0,0 +1,26 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+
+//        int myNumber = 2147483647;
+//        long longNumber = 1000000000000L; //perlu kasi L biar dy long
+//        float floatNumber = 10.01f; // perlu kasih f biar dy float
+//        double doubleNumber = 10.02;
+//        char charCharacter = 'A';
+//        boolean bool = true;
+//        String str = "Stringg";
+//
+//        System.out.println(myNumber);
+//        System.out.println(longNumber);
+//        System.out.println(floatNumber);
+//        System.out.println(doubleNumber);
+//        System.out.println(charCharacter);
+//        System.out.println(bool);
+//        System.out.println(str);
+
+        
+
+    }
+}
