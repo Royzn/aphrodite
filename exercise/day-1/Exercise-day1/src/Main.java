941fe227a39150d34740625adf97c09d2668e3c0
Royzen Anggatama <72536@fif.co.id>
2025-09-03 16:11:44 +0700

    Submit exercise 1

--- exercise/day-1/Exercise-day1/src/Main.java
+++ exercise/day-1/Exercise-day1/src/Main.java
@@ -12,7 +12,7 @@ public class Main {
         //operator yang digunakan adalah + dan -
         kueAdik += 4;
         kueAndi -=4;
-//        System.out.println("Kue Andi sekarang: " + kueAndi + " kue adik sekarang: " + kueAdik);
+        System.out.println("Kue Andi sekarang: " + kueAndi + " kue adik sekarang: " + kueAdik);
 
 //        2. **Cerita Ongkir Toko Online**
 //        Sebuah toko online pasang ongkir Rp 25.000. Saat promo, ongkir dipotong setengah. Berapa ongkir yang harus dibayar?
@@ -20,7 +20,7 @@ public class Main {
         int biayaOngkir = 25000;
         // terdapat potongan setengah artiny perlu operator / dengan 2
         biayaOngkir /= 2;
-//        System.out.println("Biaya ongkir setelah potongan: " + biayaOngkir);
+        System.out.println("Biaya ongkir setelah potongan: " + biayaOngkir);
 
 //        3. **Cerita Permen Sinta**
 //        Sinta punya 20 permen dan mau dibagi rata ke 6 temannya. Berapa sisa permen yang tidak terbagi?
@@ -30,7 +30,7 @@ public class Main {
         // dibagi rata ke 6 temannya. sisa permen bisa didapat dengan modulo terhadap 6 (%)
 
         int sisaPermen = permenSinta % 6;
-//        System.out.println("Sisa permen Sinta: " + sisaPermen);
+        System.out.println("Sisa permen Sinta: " + sisaPermen);
 
 //        4. **Cerita Point Game**
 //        Seorang pemain game punya 80 point. Setiap naik level, point bertambah 15. Setelah naik level sekali, berapa total point pemain itu?
@@ -39,13 +39,14 @@ public class Main {
         // pemain bertambah level sekali. berarti ia mendapatkan 15 poin.
 
         skorPemain+=15;
-//        System.out.println("Skor pemain sekarang: " + skorPemain);
+        System.out.println("Skor pemain sekarang: " + skorPemain);
 
 //        5. **Cerita Tiket Bioskop**
 //        Budi mau nonton film. Syaratnya, hanya boleh masuk kalau umur ≥ 18. Jika umur Budi 16, apakah dia bisa masuk?
 //        Operator apa yang digunakan untuk membandingkan umur dengan syarat tersebut?
         int umurBudi = 16;
         int umurSyaratMasuk = 18;
+        //kira menggunakan boolean untuk mengetahui apakah umur budi memenuhi syarat atau tidak
         boolean apakahBolehMasuk = (umurBudi >= umurSyaratMasuk);
 
         System.out.println("Budi " + (apakahBolehMasuk ? "Boleh" : "Tidak Boleh") + " Masuk");
